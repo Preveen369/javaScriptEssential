e022ae899544a21edcf0369de46053c666db2364
Preveen369 <spreveen123@gmail.com>
2025-03-21 10:53:44 -0400

    Modified scope files

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -1,34 +1,34 @@
-// // Global scope
-// var globalVar = "I'm a global variable";
-// let globalLet = "I'm global, but scoped with let";
-// const globalConst = "I'm a global constant";
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm global, but scoped with let";
+const globalConst = "I'm a global constant";
 
-// // Global scope
-// console.log(globalVar); // Output: "I'm a global variable"
-// console.log(globalLet); // Output: "I'm also global, but scoped with let"
-// console.log(globalConst); // Output: "I'm a global constant"
+// Global scope
+console.log(globalVar); // Output: "I'm a global variable"
+console.log(globalLet); // Output: "I'm also global, but scoped with let"
+console.log(globalConst); // Output: "I'm a global constant"
 
-// // Block scope
-// {
-//     var blockVar = "I'm a block-scoped var";
-//     let blockLet = "I'm a block-scoped let";
-//     const blockConst = "I'm a block-scoped const";
-// }
+// Block scope
+{
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+}
 
-// //Block Scope
-// console.log(blockVar);
-// console.log(blockLet);
+//Block Scope
+console.log(blockVar);
+console.log(blockLet);
 
-// function show(){
-//     var functionVar = "I'm a block-scoped var";
-//     let functionLet = "I'm a block-scoped let";
-//     const functionConst = "I'm a bock-scoped const";
-// }
-// show();
+function show(){
+    var functionVar = "I'm a block-scoped var";
+    let functionLet = "I'm a block-scoped let";
+    const functionConst = "I'm a bock-scoped const";
+}
+show();
 
-// console.log(functionVar);
-// console.log(functionLet);
-// console.log(functionConst);
+console.log(functionVar);
+console.log(functionLet);
+console.log(functionConst);
 
 {
     var practiceVar = "Practice var variable";
