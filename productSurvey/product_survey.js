3085fc30891beb416cf114d68ca578fedb5b8e79
Preveen369 <spreveen123@gmail.com>
2025-03-24 11:41:44 -0400

    Modified product survey files

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,3 +1,11 @@
+/* In this lab, you will learn how to create a user feedback form for a 
+luxury brand's products using HTML and JavaScript. You will discover 
+how to design a structured form to collect diverse user information such 
+as name, age, email, job, and product feedback. You will see how to use 
+the JavaScript functions to manage form submission, capture input values, 
+and dynamically display user-provided feedback on the webpage. You will 
+also observe event handling mechanisms. */
+
 function submitFeedback() {
     const username = document.getElementById('name').value;
     const age = document.getElementById('age').value;
@@ -6,6 +14,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('userExperience').value;
 
     alert('Thank you for your valuable feedback');
 
@@ -16,6 +25,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = userExperience;
 
     document.getElementById('userInfo').style.display = 'block';
 }
